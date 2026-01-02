b4ee3bcab9769a9a2d2351056a7e7ed9993e66f8
MatheusWith <matheuswithsilva@gmail.com>
2026-01-01 21:48:52 -0300

    Add files via upload

---
+++ helloWorld/helloworld/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val message: String = "Hello World";
+    println(message);
+    // single comment
+    /* this is
+    multi comment line
+     */
+}
