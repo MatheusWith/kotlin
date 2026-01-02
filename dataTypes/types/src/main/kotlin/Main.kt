47af122a3e3519262aa932c6d206868d01ff73b0
MatheusWith <matheuswithsilva@gmail.com>
2026-01-02 17:00:25 -0300

    Update Main.kt

--- dataTypes/types/src/main/kotlin/Main.kt
+++ dataTypes/types/src/main/kotlin/Main.kt
@@ -3,8 +3,8 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    // int
     var myint = 35;
-    // add suffix L for long integer
     var mylong = 23L;
     println("My integer ${myint}");
     println("My long integer ${mylong}");
@@ -29,6 +29,40 @@ fun main() {
     print("Smallest long value: " + l1);
     println("Largest long value: " + l2);
 
-//    var myfloat = 54F;
-//    println("My float value ${}");
+    //float
+    var myfloat = 54F;
+    println("My float value ${myfloat}");
+
+    var f1: Float = Float.MIN_VALUE;
+    var f2: Float = Float.MAX_VALUE;
+    println("Smallest Float value " + f1);
+    println("Largest Float value " + f2);
+
+    // doble
+    var d1: Double = Double.MIN_VALUE;
+    var d2: Double = Double.MAX_VALUE;
+    println("Smallest Double value " + d1);
+    println("Largest Double value " + d2);
+
+    //Bolean
+    if  (true is Boolean) {
+        println("Yes, true is bolean value");
+    };
+
+    //char
+    var aphabetical: Char = 'C';
+    println("C is a character : ${aphabetical} is Char");
+
+    //string
+    var name: String = "matheus with";
+    println("My name is $name");
+
+   //array
+    var arr: Array<String> = arrayOf("My", "Name", "Is", "With");
+    for (i in arr) {
+        println(i);
+    }
+
+
+
 }
